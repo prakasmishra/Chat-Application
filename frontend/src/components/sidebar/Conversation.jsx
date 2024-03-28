0ad928f03c6c8c83ca4136a690edee45cc97294f
Prakash Mishra <mishraprakas002@gmail.com>
2024-03-28 05:44:06 +0530

    socket part done

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,35 +1,34 @@
-// import { useSocketContext } from "../../context/SocketContext";
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversation";
 
-const Conversation = ({ conversation, lastIdx, emoji }) => {
+const Conversation = ({ conversation, lastIdx }) => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
 
 	const isSelected = selectedConversation?._id === conversation._id;
-	// const { onlineUsers } = useSocketContext();
-	// const isOnline = onlineUsers.includes(conversation._id);
+	const { onlineUsers } = useSocketContext();
+	const isOnline = onlineUsers.includes(conversation._id);
 
 	return (
 		<>
 			<div
-				className={`flex gap-2 items-center hover:bg-gray-500 rounded p-2 py-1 cursor-pointer
-				${isSelected ? "bg-gray-500" : ""}
+				className={`flex gap-2 items-center hover:bg-gray-600 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-gray-600" : ""}
 			`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
-				{/* <div className={`avatar ${isOnline ? "online" : ""}`}>
+				<div className={`avatar ${isOnline ? "online" : ""}`}>
 					<div className='w-12 rounded-full'>
-						<img src={conversation.profilePic} alt='user avatar' />
+						<img src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png' alt='user avatar' />
 					</div>
-				</div> */}
-
-				<div className='w-12 rounded-full'>
-					<img src={conversation.profilePic} alt="" />
 				</div>
+
+				{/* <div className='w-12 rounded-full'>
+					<img src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png' alt='user avatar' />
+				</div> */}
 
 				<div className='flex flex-col flex-1'>
 					<div className='flex gap-3 justify-between'>
 						<p className='font-semibold text-white'>{conversation.fullname}</p>
-						<span className='text-xl'>{emoji}</span>
 					</div>
 				</div>
 			</div>
@@ -38,5 +37,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 		</>
 	);
 };
+
+
 export default Conversation;
 
